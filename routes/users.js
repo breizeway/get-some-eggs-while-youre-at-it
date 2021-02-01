12a913f948176a5eebbc05a7f256f12fc5adecff
Jason Slade <jason.slade906@gmail.com>
2021-02-01 16:29:10 -0700

    Created User migration, set environment vairibles, started user auth in user route.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncHandler, csrfProtection } = require('./utils');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/signup', asyncHandler(async (req, res) => {
+
+  res.render('signup', { csrfToken: req.crsfToken() })
+}));
 
 module.exports = router;
