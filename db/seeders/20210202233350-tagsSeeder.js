3c908c09e6f2c1a07c7dc9cff56cf0f8ba73704f
Jason Slade <jason.slade906@gmail.com>
2021-02-02 16:51:55 -0700

    added seeder data for tagsSeeder

--- db/seeders/20210202233350-tagsSeeder.js
+++ db/seeders/20210202233350-tagsSeeder.js
@@ -2,25 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Tags', [
+      { name: 'red', user_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'orange', user_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'yellow', user_id: 3, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'green', user_id: 4, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'blue', user_id: 5, createdAt: new Date(), updatedAt: new Date() }
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+   return queryInterface.bulkDelete('Tags', null, {});
   }
 };
