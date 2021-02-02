3bc1793c5bcce6a12f800cab47f87b62468dd1f8
Jason Slade <jason.slade906@gmail.com>
2021-02-02 16:58:00 -0700

    added seeder data to contactsSeeder

--- db/seeders/20210202233543-contactsSeeder.js
+++ db/seeders/20210202233543-contactsSeeder.js
@@ -2,25 +2,20 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('UserToContacts', [
+      { user_id: 1, contact_id: 5, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 1, contact_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 1, contact_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 2, contact_id: 4, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 2, contact_id: 3, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 2, contact_id: 5, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 3, contact_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 4, contact_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 5, contact_id: 1, createdAt: new Date(), updatedAt: new Date() },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('UserToContacts', null, {});
   }
 };
