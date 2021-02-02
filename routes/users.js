a6185517711721816cccebb44783de8fa15c21fd
James Aikens <jaikens19@hotmail.com>
2021-02-02 12:44:18 -0600

    Completed error validation for user signup and login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 const { User } = require('../db/models');
 const express = require('express');
+const { check } = require('express-validator');
 const csrf = require('csurf');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-const { asyncHandler, csrfProtection } = require('./utils');
+const { asyncHandler, csrfProtection, handleValidationErrors } = require('./utils');
+
+const userSignupValidation = [
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a First Name"),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Last Name"),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a email address")
+    .isEmail()
+    .withMessage("Please provide a valid email address"),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Password"),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Password")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords must match"),
+  handleValidationErrors
+]
+
+const userLoginValidation = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a email address")
+    .isEmail()
+    .withMessage("Please provide a valid email address"),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a Password"),
+  handleValidationErrors
+]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,7 +50,10 @@ router.get('/signup', csrfProtection, asyncHandler(async (req, res) => {
   res.render('signup', { csrfToken: req.csrfToken() })
 }));
 
-router.post('/', csrfProtection, asyncHandler(async (req, res) => {
+router.post('/', 
+  userSignupValidation, 
+  csrfProtection, 
+  asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({ firstName, lastName, email, password: hashedPassword });
@@ -28,6 +67,7 @@ router.get('/login', csrfProtection, (req, res) => {
 
 router.post(
   '/login',
+  userLoginValidation,
   csrfProtection,
   asyncHandler(async (req, res) => {
     const { email, password } = req.body;
--- routes/utils.js
+++ routes/utils.js
@@ -1,6 +1,28 @@
 const csrf = require('csurf');
-
+const { validationResult } = require('express-validator')
 const csrfProtection = csrf({ cookie: true });
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
-module.exports = { asyncHandler, csrfProtection };
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map((error) => error.msg);
+
+        const err = Error("Bad request.");
+        err.errors = errors;
+        err.status = 400;
+        err.title = "Bad request.";
+        return next(err);
+    }
+    next();
+};
+
+
+
+
+
+
+
+
+module.exports = { asyncHandler, csrfProtection, handleValidationErrors };
