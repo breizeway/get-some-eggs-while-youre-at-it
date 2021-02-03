076395284a80611fa339cc723e895895e2a8e333
Jason Slade <jason.slade906@gmail.com>
2021-02-02 17:28:03 -0700

    added seeder data to tasksSeeder

--- db/seeders/20210202233728-tasksSeeder.js
+++ db/seeders/20210202233728-tasksSeeder.js
@@ -2,25 +2,26 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Tasks', [
+      { name: 'get eggs', start_date: '2021-02-02', due_date: '2021-02-03', priority: 0, estimate: 1, list_id: 1, user_id: 1, give_to_id: null, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get cheese', start_date: '2021-02-02', due_date: '2021-02-03', priority: 2, estimate: 2, list_id: 2, user_id: 1, give_to_id: 1, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get milk', start_date: '2021-02-02', due_date: '2021-02-03', priority: 3, estimate: 3, list_id: 3, user_id: 1, give_to_id: 2, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get eggs', start_date: '2021-02-02', due_date: '2021-02-03', priority: 0, estimate: 1, list_id: 1, user_id: 2, give_to_id: 3, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get cheese', start_date: '2021-02-02', due_date: '2021-02-03', priority: 2, estimate: 2, list_id: 2, user_id: 2, give_to_id: 4, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get milk', start_date: '2021-02-02', due_date: '2021-02-03', priority: 3, estimate: 3, list_id: 3, user_id: 2, give_to_id: 5, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get eggs', start_date: '2021-02-02', due_date: '2021-02-03', priority: 0, estimate: 1, list_id: 1, user_id: 3, give_to_id: null, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get cheese', start_date: '2021-02-02', due_date: '2021-02-03', priority: 2, estimate: 2, list_id: 2, user_id: 3, give_to_id: 1, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get milk', start_date: '2021-02-02', due_date: '2021-02-03', priority: 3, estimate: 3, list_id: 3, user_id: 3, give_to_id: 2, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get eggs', start_date: '2021-02-02', due_date: '2021-02-03', priority: 0, estimate: 1, list_id: 1, user_id: 4, give_to_id: 3, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get cheese', start_date: '2021-02-02', due_date: '2021-02-03', priority: 2, estimate: 2, list_id: 2, user_id: 4, give_to_id: 4, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get milk', start_date: '2021-02-02', due_date: '2021-02-03', priority: 3, estimate: 3, list_id: 3, user_id: 4, give_to_id: 5, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get eggs', start_date: '2021-02-02', due_date: '2021-02-03', priority: 0, estimate: 1, list_id: 1, user_id: 5, give_to_id: null, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get cheese', start_date: '2021-02-02', due_date: '2021-02-03', priority: 2, estimate: 2, list_id: 2, user_id: 5, give_to_id: 1, createAt: new Date(), updatedAt: new Date() },
+      { name: 'get milk', start_date: '2021-02-02', due_date: '2021-02-03', priority: 3, estimate: 3, list_id: 3, user_id: 5, give_to_id: 2, createAt: new Date(), updatedAt: new Date() },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Tasks', null, {});
   }
 };
