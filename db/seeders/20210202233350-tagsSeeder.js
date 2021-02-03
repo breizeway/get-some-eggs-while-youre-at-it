f4a803dfc6ffcec6102d52fdb01870e4aa00c91b
Jason Slade <jason.slade906@gmail.com>
2021-02-03 09:39:07 -0700

    fixed seeder data so correct relations are represented

--- db/seeders/20210202233350-tagsSeeder.js
+++ db/seeders/20210202233350-tagsSeeder.js
@@ -4,10 +4,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Tags', [
       { name: 'red', user_id: 1, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'orange', user_id: 2, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'yellow', user_id: 3, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'green', user_id: 4, createdAt: new Date(), updatedAt: new Date() },
-      { name: 'blue', user_id: 5, createdAt: new Date(), updatedAt: new Date() }
+      { name: 'orange', user_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'yellow', user_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'green', user_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'blue', user_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'violet', user_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'ultra violet', user_id: 3, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'infra red', user_id: 4, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'indigo', user_id: 5, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
--- db/seeders/20210202233709-tasksToTagsSeeder.js
+++ db/seeders/20210202233709-tasksToTagsSeeder.js
@@ -3,15 +3,15 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('TaskToTags', [
-      { task_id: 1, tag_id: 5, createdAt: new Date(), updatedAt: new Date() },
       { task_id: 1, tag_id: 1, createdAt: new Date(), updatedAt: new Date() },
       { task_id: 1, tag_id: 2, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 2, tag_id: 4, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 2, tag_id: 3, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 2, tag_id: 5, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 3, tag_id: 1, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 4, tag_id: 2, createdAt: new Date(), updatedAt: new Date() },
-      { task_id: 5, tag_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 1, tag_id: 3, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 4, tag_id: 4, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 5, tag_id: 5, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 6, tag_id: 6, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 7, tag_id: 7, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 11, tag_id: 8, createdAt: new Date(), updatedAt: new Date() },
+      { task_id: 15, tag_id: 9, createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
