73d6d2749ec16eb8431c0c0add7ce3be692e00e5
Jason Slade <jason.slade906@gmail.com>
2021-02-02 17:35:25 -0700

    added seeder data to notesSeeder

--- db/seeders/20210202233735-notesSeeder.js
+++ db/seeders/20210202233735-notesSeeder.js
@@ -2,25 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Notes', [
+      { note: 'This is a note here.', task_id: 1, user_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { note: 'This is a note here.', task_id: 4, user_id: 2, createdAt: new Date(), updatedAt: new Date() },
+      { note: 'This is a note here.', task_id: 7, user_id: 3, createdAt: new Date(), updatedAt: new Date() },
+      { note: 'This is a note here.', task_id: 11, user_id: 4, createdAt: new Date(), updatedAt: new Date() },
+      { note: 'This is a note here.', task_id: 15, user_id: 5, createdAt: new Date(), updatedAt: new Date() },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Notes', null, {});
   }
 };
