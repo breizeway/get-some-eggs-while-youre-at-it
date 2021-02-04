f7ab55f06454aa3087411b65d1863f56a09b9642
tanbre <tannor@breitigam.email>
2021-02-04 11:18:17 -0800

    started definning new lists route, new tasks route, and tasks api route

---
+++ routes/tasks.js
@@ -0,0 +1,14 @@
+const { User } = require('../db/models');
+const express = require('express');
+const { check } = require('express-validator');
+const csrf = require('csurf');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
+const { asyncHandler, csrfProtection, handleValidationErrors } = require('./utils');
+const { listData, taskData } = require('../data');
+
+// add validation for task name length later
+
+router.post('/', csrfProtection, asyncHandler(async (req, res) => {
+
+}));
