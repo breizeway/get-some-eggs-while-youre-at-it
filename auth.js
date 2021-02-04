a70adc630f9f3bf86cd1c0e4094e26c82724774d
Jason Slade <jason.slade906@gmail.com>
2021-02-04 11:58:18 -0700

    added restorUser middleware to app.js, defined in auth.js

---
+++ auth.js
@@ -0,0 +1,36 @@
+const { User } = require('./db/models');
+
+// const requireAuth = (req, res, next) => {
+//     if (!res.locals.authenticated) {
+//       return res.redirect('/users/login');
+//     }
+//     return next();
+//   };
+
+  const restoreUser = async (req, res, next) => {
+    // Log the session object to the console
+    // to assist with debugging.
+    console.log(req.session);
+
+    if (req.session.user) {
+      const { userId } = req.session.user;
+
+      try {
+        const user = await User.findByPk(userId);
+
+        if (user) {
+          res.locals.authenticated = true;
+          res.locals.user = user;
+          next();
+        }
+      } catch (err) {
+        res.locals.authenticated = false;
+        next(err);
+      }
+    } else {
+      res.locals.authenticated = false;
+      next();
+    }
+  };
+
+  module.exports = { restoreUser, }
