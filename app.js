0d2a73f5864d9f24d82474584d2654c63aae4d0d
tanbre <tannor@breitigam.email>
2021-02-05 12:12:38 -0800

    added delete buttons to list, fixed session middleware, updated list add api route to use new html

--- app.js
+++ app.js
@@ -17,12 +17,6 @@ const app = express();
 // view engine setup
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
 
@@ -37,6 +31,12 @@ app.use(
 
 // create Session table if it doesn't already exist
 store.sync();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(restoreUser);
 app.use('/', indexRouter);
