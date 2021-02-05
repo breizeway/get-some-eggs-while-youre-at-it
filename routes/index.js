3799ad1dc9b7890e21b1998cce3e4535fa892ff3
James Aikens <jaikens19@hotmail.com>
2021-02-04 20:18:33 -0600

    added redirect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/lists')
+  res.redirect('/lists');
 });
 
 module.exports = router;
