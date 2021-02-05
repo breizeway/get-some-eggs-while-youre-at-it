38ec7911c7e60d2bcb32edec3a27ebed315df20d
Jason Slade <jason.slade906@gmail.com>
2021-02-05 13:05:13 -0700

    finished front-end fetch for api/tasks/note route, untested.

--- public/javascripts/task-edit.js
+++ public/javascripts/task-edit.js
@@ -1,3 +1,34 @@
 window.addEventListener("load", (event)=> {
-  const
+    const noteForm = document.querySelector('.create-form__form');
+    const formData = new FormData(noteForm)
+    const notesContainer = document.querySelector('.notes__note-list')
+
+    noteForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const note = formData.get('note');
+        const taskId = formData.get('id');
+
+        const res = await fetch('/api/tasks/note', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ note, taskId })
+        });
+
+        const newNote = await res.json();
+
+        const subContainer = document.createElement('div');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute('id', `delete-task_${newNote.taskId}`);
+        deleteButton.setAttribute('class', 'notes__note-list__button');
+        deleteButton.innerHTML='Delete';
+
+        const noteDiv = document.createElement('div');
+        noteDiv.setAttribute('class', 'notes__note-list__div');
+        noteDiv.innerHTML = `${newNote.note}`;
+
+        subContainer.appendChild(noteDiv);
+        subContainer.appendChild(deleteButton);
+        notesContainer.appendChild(subContainer);
+    });
 })
