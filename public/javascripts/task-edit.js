9173a93d1a163c8ea854cf9b98ff2409d957b99e
Jason Slade <jason.slade906@gmail.com>
2021-02-06 08:10:39 -0700

    notes can be deleted with AJAX

--- public/javascripts/task-edit.js
+++ public/javascripts/task-edit.js
@@ -3,24 +3,29 @@ window.addEventListener("load", (event)=> {
     const formData = new FormData(noteForm)
     const notesContainer = document.querySelector('.edit-container__note-list')
     const textarea = document.getElementById('new-note')
+    const taskId = formData.get('id')
 
-    // newTasks.forEach(task => {
-    //   const rowDiv = document.createElement('div')
-    //   rowDiv.classList.add('left-container__list-row');
-    //   rowDiv.innerHTML = `
-    //     <a id="${list.id}" href="${list.href}">${list.name}</a>
-    //     <div class="left-container__delete-button-div">
-    //       <div class="left-container__delete-icon">
-    //         <i id="${list.deleteHtmlId}" class="fa fa-trash"></i>
-    //       </div>
-    //     </div>`
+    // function to create a note using AJAX
+    const createNote = async newNote => {
+     const subContainer = document.createElement('div')
+
+     subContainer.classList.add('.edit-container__note-row')
+     subContainer.innerHTML =
+     `<div class="edit-container__note-content">${newNote.note}</div>
+      <div class="edit-container__delete-button-div">
+         <div class="edit-conatianer__edit-icon"><i class="fa fa-trash" id="${newNote.id}"></i></div>
+      </div>`
+
+      notesContainer.appendChild(subContainer);
+    }
+
 
     // event listner for adding a note
     noteForm.addEventListener('submit', async (event) => {
         event.preventDefault();
 
         const note = textarea.value;
-        const taskId = formData.get('id')
+        // const taskId = formData.get('id')
         textarea.value = '';
 
         try {
@@ -31,24 +36,34 @@ window.addEventListener("load", (event)=> {
                 },
                 body: JSON.stringify({ note, taskId })
         })
+            const newNote = await res.json();
+            createNote(newNote);
 
-
-        const newNote = await res.json()
-        console.log(newNote)
-
-
-        const subContainer = document.createElement('div');
-        subContainer.classList.add('.edit-container__note-row')
-        subContainer.innerHTML =
-        `<div class="edit-container__note-content">${newNote.note}</div>
-         <div class="edit-container__delete-button-div">
-            <div class="edit-conatianer__edit-icon"><i class="fa fa-trash" id="${newNote.id}"></i></div>
-         </div>`
-        notesContainer.appendChild(subContainer);
         } catch (error) {
             console.log(error)
         }
     });
 
+    // event listener to delete a note
+    notesContainer.addEventListener('click', async event => {
+      const noteId = event.target.id;
+
+      try {
+        const res = await fetch('/api/tasks', {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ noteId, taskId })
+        })
+
+        const notes = await res.json();
+        notesContainer.innerHTML = ''
+        notes.forEach(note => createNote(note))
+
+      } catch (error) {
+        console.error(error)
+      }
+    })
 
 })
